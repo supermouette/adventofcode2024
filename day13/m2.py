4cbf184dcbd5f5c543a442a29ae54f20d9b906ae
Loïc Souverain <loic.souverain@gmail.com>
2024-12-15 10:45:46 +0100

    day13

---
+++ day13/m2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+with open("input.txt") as f:
+    lines = [l.strip("\n") for l in f.readlines()]
+
+prize = 0
+for i in range(len(lines) // 4 + 1):
+    extract = lines[i * 4 : i * 4 + 3]
+    ax, ay = [int(c[2:]) for c in extract[0][len("Button A: ") :].split(", ")]
+    bx, by = [int(c[2:]) for c in extract[1][len("Button B: ") :].split(", ")]
+    px, py = [
+        10000000000000 + int(c[2:]) for c in extract[2][len("Prize: ") :].split(", ")
+    ]
+
+    a, b = np.linalg.solve(np.array([[ax, bx], [ay, by]]), np.array([px, py]))
+
+    a = int(round(a))
+    b = int(round(b))
+    if px == ax * a + bx * b and py == ay * a + by * b:
+        prize += a * 3 + b
+
+print(prize)
